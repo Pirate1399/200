54772baa8288afb65c06b9340643b30c0f2d67b2
Samovol <samovole@gmail.com>
2022-07-09 15:16:25 +0300

    Добавил тесты на count

--- test_linkedlist.py
+++ test_linkedlist.py
@@ -1,9 +1,23 @@
-# fixme unittest
+import unittest
+
+from Списки import LinkedList
+
+class TestCase(unittest):
+
+    def test_count_from_empty_list(self):
+        value = 10
+        ll = LinkedList()
+        message = "Значение должно быть 0"
+        self.assertEqual(ll.count(value), 0, message)
+        """ Количество от пустого списка должно быть 0. """
+
+    def test_count_from_list(self):
+        value = 1
+        ll = LinkedList([1,2,1,5,6,7])
+        message = "Что-то пошло не так, должно быть 2"
+        self.assertEqual(ll.count(value), 2, message)
+        """ Количество искомого элемента в списке. """
 
 
-def count_from_empty_list():
-    """ Количество от пустого списка должно быть 0. """
 
 
-def count_from_list():
-    """ Количество искомого элемента в списке. """
